46d48a02c06659ba53422078b54c600c6d2405c8
Jack1378 <765155530@qq.com>
2017-07-10 10:07:25 +0800

    我修改了  wancheng

--- lifeiyu20170710/src/main/java/com/example/baiwei/lifeiyu20170710/MainActivity.java
+++ lifeiyu20170710/src/main/java/com/example/baiwei/lifeiyu20170710/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.baiwei.lifeiyu20170710;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AmountView amount_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
 
         System.out.println("-------------");
+    }
+
+    private void initView() {
+        amount_view = (AmountView) findViewById(R.id.amount_view);
+        amount_view.setGoods_storage(50);
+        amount_view.setOnAmountChangeListener(new AmountView.OnAmountChangeListener() {
+            @Override
+            public void onAmountChange(View view, int amount) {
+                Toast.makeText(getApplicationContext(), "Amount=>  " + amount, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
